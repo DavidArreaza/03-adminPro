d557a76950cc8366a823881714a2a41cf1d59518
David Arreaza Gil <darreazagil@gmail.com>
2022-03-10 14:03:26 +0100

    Validaciones Form
    Validaciones:
    - Nombre
    - Email
    - Pass
    - Check

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,8 +14,10 @@ export class RegisterComponent{
     nombre : [ 'David', [ Validators.required] ],
     email : [ 'David@email.com', [ Validators.required, Validators.email] ],
     password : [ '123456', [ Validators.required] ],
-    passwordConfirmed : [ '123456', [ Validators.required] ],
+    passwordConfirmed : [ '1234567', [ Validators.required] ],
     terminos : [ true, Validators.required ]
+  }, {
+    validators: this.passwordIguales('password', 'passwordConfirmed')
   });
 
   constructor( private fb : FormBuilder) { }
@@ -23,8 +25,20 @@ export class RegisterComponent{
   crearUsuario(){
     this.formSubmitted = true;
     console.log( this.registerForm.value );
+
+    if( this.registerForm.valid ){
+      console.log("Formulario correcto");
+    }else{
+      console.log("Formulario incorrecto");
+    }
+
   }
 
+  /**
+   * Validar campos
+   * @param campo 
+   * @returns 
+   */
   campoNoValido(campo : string): boolean{
     
     if( this.registerForm.get(campo)?.invalid && this.formSubmitted){
@@ -32,7 +46,53 @@ export class RegisterComponent{
     }else{
       return false;
     }
-    
+  }
+
+  /**
+   * Validar passwords
+   * @returns true si son iguales - false si no son iguales
+   */
+  validarPassword(){
+
+    const pass1 =  this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('passwordConfirmed')?.value;
+
+    if(pass1 === pass2){
+      return false;
+    }else{
+      return true;
+    }
+
+  }
+
+  /**
+   * Comprobar chek
+   * @returns true si marca check. False no marca check
+   */
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
+
+  /**
+   * Validar que las passwords sean iguales
+   * @param pass1 
+   * @param pass2 
+   * @returns 
+   */
+  passwordIguales(pass1: string, pass2: string){
+
+    return ( formGroup : FormGroup ) => {
+
+      const pass1Control = formGroup.get(pass1);
+      const pass2Control = formGroup.get(pass2);
+
+        if( pass1Control?.value === pass2Control?.value ){
+          pass2Control?.setErrors(null);
+        } else {
+          pass2Control?.setErrors({noEsIgual : true});
+        }
+    }
+
   }
 
 }
