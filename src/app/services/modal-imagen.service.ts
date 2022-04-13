d22bc6640434c2bf157be00b83baa8b0db31d266
David Arreaza Gil <darreazagil@gmail.com>
2022-04-13 17:05:51 +0200

    ModalService

---
+++ src/app/services/modal-imagen.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalImagenService {
+
+
+  private _ocultarModal : boolean = true;
+  public tipo : any;
+  public id : string = '';
+  public img : string = 'no-image';
+
+  public updateImagen : EventEmitter<string> = new EventEmitter<string>();
+
+  get ocultarModal(){
+    return this._ocultarModal;
+  }
+
+  abrirModal( tipo: 'usuarios'|'medicos'|'hospitales', id : any, img?: string ){
+    this._ocultarModal = false;
+    this.tipo = tipo;
+    this.id = id;
+    
+    if( img?.includes('https') ){
+      this.img = img;
+    } else {
+      this.img = `${ base_url }/upload/${ tipo }/${ img }`
+    }
+  }
+
+  cerrarModal(){
+    return this._ocultarModal = true;
+  }
+
+  constructor() { }
+}
