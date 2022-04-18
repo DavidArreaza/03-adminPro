1c06b3954a45831befa95dc7199ac276770c7159
David Arreaza Gil <darreazagil@gmail.com>
2022-04-18 17:09:21 +0200

    Model Hospital

---
+++ src/app/models/hospital.model.ts
@@ -0,0 +1,29 @@
+interface _hospitalUser {
+    _id : string,
+    nombre : string,
+    img : string
+}
+
+export class Hospital {
+
+    constructor(
+        public _id?         : string,
+        public nombre       : string = "",
+        public image?       : string,
+        public usuario?     : _hospitalUser
+    ){};
+
+    // get imagenUrl(){
+
+    //     if( !this.image ){
+    //         return `${base_url}/upload/usuarios/no-image`
+    //     } else if ( this.image?.includes('https') ){
+    //         return this.image;
+    //     } else if ( this.image ){
+    //         return `${base_url}/upload/usuarios/${this.image}`
+    //     } else {
+    //         return `${base_url}/upload/usuarios/no-image`
+    //     }        
+    // }
+
+}
