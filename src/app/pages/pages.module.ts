a07b31ac0695c1039f903d2aab5cdd5279930529
David Arreaza Gil <darreazagil@gmail.com>
2022-04-21 15:37:06 +0200

    Modules

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -17,8 +17,7 @@ import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component'
 import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
 import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
 import { PipesModule } from '../pipes/pipes.module';
-
-
+import { MedicoComponent } from './mantenimientos/medicos/medico.component';
 
 
 @NgModule({
@@ -34,6 +33,7 @@ import { PipesModule } from '../pipes/pipes.module';
     UsuariosComponent,
     HospitalesComponent,
     MedicosComponent,
+    MedicoComponent,
   ],
   exports: [
     DashboardComponent,
