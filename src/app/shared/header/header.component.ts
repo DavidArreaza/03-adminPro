902b1cc61796a53639637c44c81734a0f8f8598d
David Arreaza Gil <darreazagil@gmail.com>
2022-05-10 08:32:44 +0200

    Buscar All

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -10,12 +11,23 @@ export class HeaderComponent{
 
   public usuario;
 
-  constructor( private usuarioService : UsuarioService ) { 
+  constructor( private usuarioService : UsuarioService, private router : Router ) { 
     this.usuario = this.usuarioService.usuario;
   }
 
   logout(){
     this.usuarioService.logout();
+  }
+
+  buscar(termino : string){
+
+    if(termino.length === 0){
+      this.router.navigateByUrl(`/dashboard`)
+    }else{
+      this.router.navigateByUrl(`/dashboard/buscar/${termino}`)
+    }
+
+    console.log(termino)
   }
 
 }
